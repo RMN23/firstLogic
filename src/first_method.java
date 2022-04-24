0f70b655bd658a861eb561321d09318de37d20a1
RMN <zherebetskyi@gmail.com>
2022-04-24 15:09:20 +0300

    first simple logic

---
+++ src/first_method.java
@@ -0,0 +1,11 @@
+public class first_method {
+
+    static void firstLogic(String bla) {
+        if(bla == "Lubko durak"){
+            System.out.println("It works");
+        }
+        else if (bla != "Lubko durak"){
+            System.out.println("Error");
+        }
+    }
+}
---
+++ src/main.java
@@ -0,0 +1,8 @@
+public class main extends first_method{
+
+    public static void main(String[] args) {
+        firstLogic("Lubko durak");
+    }
+}
+
+
